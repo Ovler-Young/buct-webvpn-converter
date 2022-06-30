be5bf6d2222306597c505e2dda667a18fee335da
Ovler <ovlertheyoung@gmail.com>
2022-06-30 22:27:56 +0800

    Upd for buct

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,7 @@ const Footer = () => (
   <footer className="bg-zinc-500/10 p-4 text-center text-gray-400 text-sm">
     Created with love at BUCT
     <br />
+    Source code by{' '}
     <a
       href="https://spencerwoo.com"
       target="_blank"
@@ -9,6 +10,16 @@ const Footer = () => (
       className="underline opacity-80 hover:opacity-100 transition-all duration-150"
     >
       Spencer Woo
+    </a>{' '}
+    <br />
+    Edited by{' '}
+    <a
+      href="https://180811.xyz"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="underline opacity-80 hover:opacity-100 transition-all duration-150"
+    >
+    Ovler
     </a>{' '}
     @ {new Date().getFullYear()}
   </footer>
